0fef538358555a1d504cb7d9c1f66f1a4910f761
Daniel R. Koris <korisd@gmail.com>
2018-10-06 12:38:57 -0600

    Refs m241dan/HyperloopSim#4: Receiver and Echoer
    See issue for implementation details

--- sim_recv.py
+++ sim_recv.py
@@ -1,5 +1,7 @@
 import socket
 import struct
+import packet
+import time
 
 IP = "127.0.0.1"
 PORT = 5005
@@ -10,8 +12,7 @@ sock.bind((IP, PORT))
 
 while True:
     data, addr = sock.recvfrom(1024)
-    data = struct.unpack( 'Biiiiiii', data )
-    output = ""
-    for value in data:
-	output += str(value) + "\n"
-    print "received message: ", output
+    print( "Receiving..." )
+    data = packet.Packet(data)
+    print( str(data) )
+    print( "Receive Complete at " + str(time.time()) )
